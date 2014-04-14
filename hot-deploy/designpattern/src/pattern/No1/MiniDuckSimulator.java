78818e08eeecfe9cdb5cad4e82c84a274a047861
lianghh <nggliang@gmail.com>
2014-04-14 22:23:10 +0800

    之前写的代码搬迁

--- hot-deploy/designpattern/src/pattern/No1/MiniDuckSimulator.java
+++ hot-deploy/designpattern/src/pattern/No1/MiniDuckSimulator.java
@@ -1,11 +1,14 @@
 package pattern.No1;
 
+import javolution.util.FastMap;
 import org.ofbiz.service.DispatchContext;
 import pattern.No1.achieve.MallardDuck;
 import pattern.No1.achieve.ModelDuck;
 import pattern.No1.achieve.implachieve.FlyRocketPowered;
 import pattern.No1.main.Duck;
 
+import java.lang.Object;
+import java.lang.String;
 import java.util.Map;
 
 import org.ofbiz.service.ServiceUtil;
@@ -19,12 +22,17 @@ public class MiniDuckSimulator {
      * 测试代码入口
      */
     public static Map<String, Object> run(DispatchContext dctx, Map<String, ? extends Object> context) {
+
+        Map<String,Object> resultMap = FastMap.newInstance();
+
         Duck model = new ModelDuck();
         model.performFly();
         model.setFlyBehavior(new FlyRocketPowered());
         model.performFly();
 
-        return ServiceUtil.returnSuccess();
+        resultMap.put("result","OK");
+
+        return resultMap;
 
     }
 

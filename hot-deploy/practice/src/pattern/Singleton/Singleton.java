bd5da503ae06d80e39b910d8a11101236081d564
lianghh <nggliang@gmail.com>
2014-04-25 22:28:17 +0800

    del     test

---
+++ hot-deploy/practice/src/pattern/Singleton/Singleton.java
@@ -0,0 +1,29 @@
+package pattern.Singleton;
+
+/**
+ * Created by root on 14-4-25.
+ */
+public class Singleton {
+
+    /**
+     * 确保类只被实例化一次
+     */
+    private volatile static Singleton uniqueInstance;
+
+    /**
+     * 外部程序无法实例化该类
+     */
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(uniqueInstance == null){
+            synchronized (Singleton.class){
+                if(uniqueInstance == null){
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+}

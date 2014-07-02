83d51f4ba043465115ff61a63499b0860f921f49
lianghh <nggliang@gmail.com>
2014-07-02 21:11:15 +0800

    双列表 改为 按钮组 显示

--- hot-deploy/std-ecommerce/webapp/std-ecommerce/WEB-INF/actions/product/listProducts-collapse.groovy
+++ hot-deploy/std-ecommerce/webapp/std-ecommerce/WEB-INF/actions/product/listProducts-collapse.groovy
@@ -1,22 +1,17 @@
-import org.ofbiz.base.util.Debug
 import org.ofbiz.entity.util.EntityUtil
 import javolution.util.FastList
-import javolution.util.FastMap
 
-prodCatalogCategorys = EntityUtil.filterByDate(delegator.findByAnd("ProdCatalogCategory",[prodCatalogId:"STDCatalog",prodCatalogCategoryTypeId:"PCCT_BROWSE_ROOT"]))
+prodCatalogCategorys = EntityUtil.filterByDate(delegator.findByAndCache("ProdCatalogCategory",[prodCatalogId:"STDCatalog",prodCatalogCategoryTypeId:"PCCT_BROWSE_ROOT"]))
 prodCatalogCategory = EntityUtil.getFirst(prodCatalogCategorys)
 
 /**
  * 通过闭包迭代取得所有分类以及下属分类
  */
 getAllSubProductCategory = { parentProductCategoryId,productCategoryList ->
-    productCategoryRollups = EntityUtil.filterByDate(delegator.findByAnd("ProductCategoryRollup",[parentProductCategoryId:parentProductCategoryId]))
+    productCategoryRollups = EntityUtil.filterByDate(delegator.findByAndCache("ProductCategoryRollup",[parentProductCategoryId:parentProductCategoryId]))
     productCategoryRollups.each{ productCategoryRollup ->
-        productCategory = delegator.findOne("ProductCategory",[productCategoryId:productCategoryRollup.productCategoryId],false)
+        productCategory = delegator.findOne("ProductCategory",[productCategoryId:productCategoryRollup.productCategoryId],true)
         productCategoryList.add(productCategory)
-//        subProductCategoryList = FastList.newInstance()
-//        productCategory.put("productCategoryList",subProductCategoryList)
-//        getAllSubProductCategory(productCategoryRollup.productCategoryId,subProductCategoryList)
     }
 }
 
--- hot-deploy/std-ecommerce/webapp/std-ecommerce/product/listProducts-collapse.ftl
+++ hot-deploy/std-ecommerce/webapp/std-ecommerce/product/listProducts-collapse.ftl
@@ -1,22 +1,5 @@
-<div class="panel-group well" id="accordion">
-
+<div class="btn-group-vertical" style="width: 100%">
     <#list productCategoryList as productCategory>
-
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <a data-toggle="collapse" data-parent="#accordion" href="#${(productCategory.productCategoryId)!}">
-                    <h4 class="panel-title">
-                        ${(productCategory.description)!}
-                    </h4>
-                </a>
-            </div>
-            <div id="${(productCategory.productCategoryId)!}" class="panel-collapse collapse">
-                <div class="panel-body">
-                    子分类
-                </div>
-            </div>
-        </div>
-
+        <button type="button" class="btn btn-default">${(productCategory.description)!}</button>
     </#list>
-
 </div>
